42118029c1037ff792a9bb5969472540ed3ed541
douglasalbertsantos <douglasalbert7@outlook.com>
2025-01-19 17:17:21 -0300

    até ex54

---
+++ ex54.py
@@ -0,0 +1,13 @@
+maior = 0
+menor = 0
+n1 = 0
+
+for c in range(1, 8):
+    n1 = int(input(f"Em que ano a {c}ª pessoa nasceu: "))
+    if n1 > 2007:
+        menor += 1
+    else:
+        maior += 1
+
+print(f'Ao todo tivemos {menor} pessoas menor de idade')
+print(f'e {maior} pessoas maior de idade')    
